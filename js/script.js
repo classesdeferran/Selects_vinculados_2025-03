71355d0e3c3b22d1f788da5081f07a124ad0b1f0
Ferran <ferran.docencia@gmail.com>
2025-03-25 11:53:53 +0100

    2025-03-25

--- js/script.js
+++ js/script.js
@@ -25,15 +25,40 @@ const form1 = document.forms['form1']
 // Obtener los select a cambiar
 const ciudad = document.getElementById("ciudad")
 const pais = document.getElementById("pais")
+const continente = document.getElementById('continente')
+
+
+continente.addEventListener('change', () => {
+    const continenteSeleccionado = form1.continente.value 
+    // console.log(continenteSeleccionado);
+    let paisesHTML = ""
+    for (continentePaises in paises) {
+        if (continentePaises == continenteSeleccionado) {
+            const paisesObtenidos = paises[continentePaises]
+            paisesObtenidos.forEach((pais) => {
+                paisesHTML += `<option value="${pais}">${pais}</option>`
+            })
+            break
+        }
+    }
+    pais.innerHTML = paisesHTML
+    actualizaCiudad(form1.pais.value)
+
+})
 
 // Cuando cambia el pais hay que actualizar la lista de ciudades
 pais.addEventListener("change", () => {
     const paisSeleccionado = form1.pais.value
+    console.log(paisSeleccionado);
+    actualizaCiudad(paisSeleccionado)
+})
+
+function actualizaCiudad( pais) {
     // console.log(paisSeleccionado);
     let ciudadesHTML = ""
     for (paisCiudades in ciudades) {
         // console.log(paisCiudades);
-        if (paisSeleccionado == paisCiudades) {
+        if (pais == paisCiudades) {
             const ciudadesObtenidas = ciudades[paisCiudades];
             ciudadesObtenidas.forEach((ciudad) => {
                 ciudadesHTML += `<option value="${ciudad}">${ciudad}</option>`
@@ -42,4 +67,4 @@ pais.addEventListener("change", () => {
         }
     }
     ciudad.innerHTML = ciudadesHTML
-})
+}
